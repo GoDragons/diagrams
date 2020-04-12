37bda422aedd07580b8273dd680843fc3f8469bb
dragos <dragos@dragoss-MacBook-Pro.local>
2020-04-12 19:48:05 +0100

    Commente out everything but essentials for createroom

--- simple-websockets-chat-app/createroom/app.js
+++ simple-websockets-chat-app/createroom/app.js
@@ -1,29 +1,29 @@
 // Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
-const AWS = require("aws-sdk");
+// const AWS = require("aws-sdk");
 
-const ddb = new AWS.DynamoDB.DocumentClient({
-  apiVersion: "2012-08-10",
-  region: process.env.AWS_REGION,
-});
+// const ddb = new AWS.DynamoDB.DocumentClient({
+//   apiVersion: "2012-08-10",
+//   region: process.env.AWS_REGION,
+// });
 
 exports.handler = async (event) => {
-  const putParams = {
-    TableName: process.env.ROOMS_TABLE_NAME,
-    Item: {
-      roomId: event.data,
-    },
-  };
-
-  try {
-    await ddb.put(putParams).promise();
-  } catch (err) {
-    return {
-      statusCode: 500,
-      body: "Failed to create room: " + JSON.stringify(err),
-    };
-  }
+  // const putParams = {
+  //   TableName: process.env.ROOMS_TABLE_NAME,
+  //   Item: {
+  //     roomId: event.data,
+  //   },
+  // };
+  console.log("createroom function called, event:", event);
+  // try {
+  //   await ddb.put(putParams).promise();
+  // } catch (err) {
+  //   return {
+  //     statusCode: 500,
+  //     body: "Failed to create room: " + JSON.stringify(err),
+  //   };
+  // }
 
   return { statusCode: 200, body: "Room created." };
 };
