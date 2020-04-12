45356061a5b72bbfc7d34e239604df3657d77c78
dragos <dragos@dragoss-MacBook-Pro.local>
2020-04-12 18:36:13 +0100

    Remove terraform code

--- terraform/api_gateway.tf
+++
@@ -1,7 +0,0 @@
-
-
-resource "aws_apigatewayv2_api" "diagrams" {
-  name                       = "diagrams-websocket-api"
-  protocol_type              = "WEBSOCKET"
-  route_selection_expression = "$request.body.action"
-}
--- terraform/cloudwatch.tf
+++
@@ -1,6 +0,0 @@
-# resource "aws_cloudwatch_log_group" "diagrams" {
-#   name              = "/aws/lambda/diagrams"
-#   retention_in_days = 14
-# }
-
-# See also the following AWS managed policy: AWSLambdaBasicExecutionRole
