868cdad4497c08f89abb5b0864958bd0fba3488a
dragosiordachioaia <xdragosy@gmail.com>
2020-06-05 10:45:23 +0100

    Create page structure for DiagramDetails

--- frontend/src/DiagramDetails/DiagramDetails.jsx
+++ frontend/src/DiagramDetails/DiagramDetails.jsx
@@ -1,6 +1,17 @@
 import React, { useState, useEffect } from "react";
 
 import axios from "axios";
+import {
+  Empty,
+  List,
+  Row,
+  Col,
+  Button,
+  Input,
+  Typography,
+  Spin,
+  Timeline,
+} from "antd";
 import { REST_API_URL } from "common/constants";
 import { withRouter } from "react-router-dom";
 
@@ -8,6 +19,8 @@ import "./DiagramDetails.scss";
 
 export function DiagramDetails({ userCredentials, userData, match }) {
   const [infoRequested, setInfoRequested] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+
   console.log("loaded");
   useEffect(() => {
     if (!infoRequested) {
@@ -28,12 +41,29 @@ export function DiagramDetails({ userCredentials, userData, match }) {
         }
       )
       .then((response) => {
+        setLoaded(true);
         console.log("diagramDetails:", response.data);
       })
       .catch((e) => console.log(`Could not get diagrams:`, e));
   }
 
-  return <p>DiagramDetails</p>;
+  return (
+    <div className="diagram-details-page">
+      {!loaded ? (
+        <div className="empty-page">
+          <Spin />
+        </div>
+      ) : (
+        <>
+          <Row className="main-content">
+            <Col span={6}>
+              <p>diagram details</p>
+            </Col>
+          </Row>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default withRouter(DiagramDetails);
